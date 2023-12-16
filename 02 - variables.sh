b37d80e8a06c498a447775a31e92fdfec219258b
saibhargav <saibhargavyadlapalli609@gmail.com>
2023-12-16 12:58:32 +0530

    purpose

--- 02 - variables.sh
+++ 02 - variables.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
- number=$1
+ NUMBER=$1
 
- if [ $number -gt 100 ];
+ if [ $NUMBER -gt 100 ];
  then
-   echo "given number $number is greater than 100"
+   echo "given number $NUMBER is greater than 100"
  else
-   echo "given number $number is not greater than 100"
+   echo "given number $NUMBER is not greater than 100"
 fi  
